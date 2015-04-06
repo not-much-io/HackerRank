93de8c30efe9e3d415ee09d459ece9134088d47e
Kristo Koert <kristo.koert@gmail.com>
2015-04-06 22:34:03 +0300

    Update and rename angry-children.clj to max-min.clj

--- algorithms/max-min.clj
+++ algorithms/max-min.clj
@@ -8,20 +8,13 @@
 
 (defn min-unfairness 
   ([sequence]
-   (min-unfairness sequence 
-                   0
-                   (Integer/MAX_VALUE)))
+   (min-unfairness sequence 0 (Integer/MAX_VALUE)))
   ([sequence indx min-unf]
    (if (> (+ sub-seq-len indx) seq-len)  
      min-unf
-     (let [new-unf (- (nth sequence
-                           (dec (+ indx
-                                   sub-seq-len)))
-                      (nth sequence
-                           indx))
-           new-min-unf (if (< new-unf min-unf)
-                         new-unf
-                         min-unf)
+     (let [new-unf (- (nth sequence (dec (+ indx sub-seq-len)))
+                      (nth sequence indx))
+           new-min-unf (if (< new-unf min-unf) new-unf min-unf)
            new-indx (inc indx)]
        (recur sequence new-indx new-min-unf)))))
 
