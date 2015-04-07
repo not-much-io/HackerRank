b887443a82ac71b40207e0d20433014934de848b
KristoKoert <kristo.koert@gmail.com>
2015-04-07 14:28:31 +0300

    Super digit solution

---
+++ functional-programming/super-digit.clj
@@ -0,0 +1,24 @@
+(ns super-digit)
+
+(defn- char||str->int [c]
+  (Integer/parseInt (str c)))
+
+(defn- digits-sum [val]
+  (apply + (map char||str->int val)))
+
+(defn super-digit [val]
+  (if (= 1 (count val))
+    val
+    (let [sum (digits-sum val)]
+      (recur (str sum)))))
+
+(let [[n k]       (clojure.string/split (read-line) #" ")
+      init-sum    (digits-sum n)
+      initial-val (str (* (char||str->int k) init-sum))]
+  (println (super-digit initial-val)))
+
+
+
+
+
+
